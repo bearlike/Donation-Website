761606ee758d70e883c03e02aa931bca50a44e0a
Krishna Alagiri (KK) <krishna.alagiri03@gmail.com>
2019-06-18 02:01:44 +0530

    Minor Issues

---
+++ common/header.php
@@ -0,0 +1,16 @@
+<header id="luxbar" class="luxbar-fixed">
+  <input type="checkbox" class="luxbar-checkbox" id="luxbar-checkbox" />
+  <div class="luxbar-menu luxbar-menu-right luxbar-menu-dark">
+    <ul class="luxbar-navigation">
+      <li class="luxbar-header">
+        <a href="#" class="luxbar-brand"><img src="../assets/img/logo.png" style="height: 80%" alt="Bhumi Logo"></a> <label class="luxbar-hamburger luxbar-hamburger-doublespin" id="luxbar-hamburger" for="luxbar-checkbox">
+          <span></span>
+        </label>
+      </li>
+      <li class="luxbar-item"><a href="../index.html">Home</a></li>
+      <li class="luxbar-item"><a href="../public/about.html">About</a></li>
+      <li class="luxbar-item"><a href="../public/search.php">Children Page</a></li>
+      <li class="luxbar-item"><a href="../public/general-donation.html">General Donation</a></li>
+    </ul>
+  </div>
+</header>
